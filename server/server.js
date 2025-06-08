0b98ea05ae68a31968add91d9f1dd6ce11db7552
Ishan Awasthi <118539441+Ishan678-code@users.noreply.github.com>
2025-06-08 21:10:16 +0545

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ await connectDB();
 await connectCloudinary();
 
 //allow multiple origins
-const allowedOrigins = ["http://localhost:5173","https://greencart-rho-eight.vercel.app"];
+const allowedOrigins = ["http://localhost:5173","https://grocery-management-system-two.vercel.app"];
 
 app.post('/stripe', express.raw({type: 'application/json'}),stripeWebhooks)
 //Middleware configuration
